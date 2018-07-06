513e60a375ff071dd6bdadb4cb908958d4330794
Gerardo Cordero <cordero.gerard@gmail.com>
2018-07-05 22:49:29 -0500

    MenuComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ServiciosComponent } from './servicios/servicios.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { CardsComponent } from './components/cards/cards.component';
+import { ListComponent } from './components/list/list.component';
+import { FormComponent } from './components/form/form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServiciosComponent
+    ServiciosComponent,
+    MenuComponent,
+    CardsComponent,
+    ListComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule
